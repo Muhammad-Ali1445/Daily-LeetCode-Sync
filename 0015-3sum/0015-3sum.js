d879bb93cdd1f04283b0757dba4d20e1d3d4cd67
Muhammad-Ali1445 <108221222+Muhammad-Ali1445@users.noreply.github.com>
2024-07-13 23:39:58 +0500

    Time: 167 ms (40.7%), Space: 64.4 MB (85.9%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,31 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function(arr) {
-     let resArr = [];
-  let uniqueSet = new Set();
-  arr.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
-
-    while (left < right) {
-      let sum = arr[i] + arr[left] + arr[right];
-      if (sum == 0) {
-        let triplet = [arr[i], arr[left], arr[right]];
-        let tripletStr = JSON.stringify(triplet);
-        if (!uniqueSet.has(tripletStr)) {
-          uniqueSet.add(tripletStr);
-          resArr.push(triplet);
-          // console.log(resArr);
+     arr.sort((a, b) => a - b);
+    let resArr = [];
+    
+    for (let i = 0; i < arr.length - 2; i++) {
+      if (i > 0 && arr[i] === arr[i - 1]) continue;
+  
+      let left = i + 1;
+      let right = arr.length - 1;
+      
+      while (left < right) {
+        let sum = arr[i] + arr[left] + arr[right];
+        
+        if (sum === 0) {
+          resArr.push([arr[i], arr[left], arr[right]]);
+          
+          while (left < right && arr[left] === arr[left + 1]) left++; 
+          while (left < right && arr[right] === arr[right - 1]) right--; 
+          
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          right--;
         }
-        left++;
-        right--;
-      } else if (sum < 0) {
-        left++;
-      } else {
-        right--;
       }
     }
-  }
-  return resArr;
+  
+    return resArr;
 };
