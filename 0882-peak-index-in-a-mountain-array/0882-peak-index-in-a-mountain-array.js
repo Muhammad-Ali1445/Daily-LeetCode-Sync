1644fb2956b6050077fa067a3a3104ff8f7f2808
Muhammad-Ali1445 <108221222+Muhammad-Ali1445@users.noreply.github.com>
2024-07-23 11:40:08 +0500

    Time: 66 ms (60.42%), Space: 58.4 MB (65.15%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    let mid;
+    while (start < end) {
+        mid = Math.floor((start + end) / 2);
+        if (arr[mid] < arr[mid + 1]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return start;
+};
