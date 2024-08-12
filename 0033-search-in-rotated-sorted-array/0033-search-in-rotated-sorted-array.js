f2bd91cbba9e7dbf01eba8da3ff893fb2a9d6dc2
Muhammad-Ali1445 <108221222+Muhammad-Ali1445@users.noreply.github.com>
2024-08-12 11:04:18 +0500

    Time: 51 ms (69.39%), Space: 49.3 MB (12.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,49 +3,44 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (arr, target) {
+function search(arr, target) {
     function getPivot(arr) {
-        let s = 0;
-        let e = arr.length - 1;
-        let mid;
-        let firstIndex = arr[0];
-        while (s < e) {
-            mid = Math.floor((s + e) / 2);
-            if (arr[mid] >= firstIndex) {
-                s = mid + 1;
-            } else {
-                e = mid;
-            }
+      let s = 0;
+      let e = arr.length - 1;
+      let mid;
+      let firstIndex = arr[0];
+      while (s < e) {
+        mid = Math.floor((s + e) / 2);
+        if (arr[mid] >= firstIndex) {
+          s = mid + 1;
+        } else {
+          e = mid;
         }
-        return s;
+      }
+      return s;
     }
 
     function binarySearch(arr, start, end, target) {
-        let s = start;
-        let e = end;
-        let mid;
-        while (s <= e) {
-            mid = Math.floor((s + e) / 2);
-            if (arr[mid] === target) {
-                return mid;
-            } else if (target > arr[mid]) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    function findElementInRotatedArr(arr, target) {
-        let pivot = getPivot(arr);
-        if (target >= arr[pivot] && target <= arr[arr.length - 1]) {
-            return binarySearch(arr, pivot, arr.length - 1, target);
+      let s = start;
+      let e = end;
+      let mid;
+      while (s <= e) {
+        mid = Math.floor((s + e) / 2);
+        if (arr[mid] === target) {
+          return mid;
+        } else if (target > arr[mid]) {
+          s = mid + 1;
         } else {
-            return binarySearch(arr, 0, pivot - 1, target);
+          e = mid - 1;
         }
+      }
+      return -1;
     }
-    return findElementInRotatedArr(arr, target);
-};
 
-
+    let pivot = getPivot(arr);
+    if (target >= arr[pivot] && target <= arr[arr.length - 1]) {
+      return binarySearch(arr, pivot, arr.length - 1, target);
+    } else {
+      return binarySearch(arr, 0, pivot - 1, target);
+    }
+  }
