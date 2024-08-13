33b67b024f013dff7e9d0604a93956274cc84053
Muhammad-Ali1445 <108221222+Muhammad-Ali1445@users.noreply.github.com>
2024-08-13 23:11:54 +0500

    Time: 128 ms (25%), Space: 61.3 MB (14.29%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.js
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.js
@@ -3,30 +3,32 @@
  * @param {number[]} quantities
  * @return {number}
  */
-var minimizedMaximum = function(stores, quantities) {
-    function isPossibleSol(quantities, mid, stores) {
+
+function isPossibleSol(quantities, mid, stores) {
     let storeCount = 0;
     for (let i = 0; i < quantities.length; i++) {
-      storeCount += Math.ceil(quantities[i] / mid);
-      if (storeCount > stores) {
-        return false;
-      }
+        storeCount += Math.ceil(quantities[i] / mid);
+        if (storeCount > stores) {
+            return false;
+        }
     }
     return true;
-  }
-  let s = 1;
-  let mid;
-  let ans;
-  let totalSum = quantities.reduce((acc, curr) => acc + curr, 0);
-  let e = totalSum;
-  while (s <= e) {
-    mid = Math.floor((s + e) / 2);
-    if (isPossibleSol(quantities, mid, stores)) {
-      ans = mid;
-      e = mid - 1;
-    } else {
-      s = mid + 1;
+}
+var minimizedMaximum = function (stores, quantities) {
+
+    let s = 1;
+    let mid;
+    let ans;
+    let totalSum = quantities.reduce((acc, curr) => acc + curr, 0);
+    let e = totalSum;
+    while (s <= e) {
+        mid = Math.floor((s + e) / 2);
+        if (isPossibleSol(quantities, mid, stores)) {
+            ans = mid;
+            e = mid - 1;
+        } else {
+            s = mid + 1;
+        }
     }
-  }
-  return ans;
+    return ans;
 };
