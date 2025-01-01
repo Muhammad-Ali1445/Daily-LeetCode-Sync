f8bc3d548c013a29aa5381523785542619197b8b
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-01-01 12:52:41 +0500

    Time: 4 ms (76.08%), Space: 56 MB (84.69%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -3,10 +3,9 @@
  * @return {number[]}
  */
 var sortedSquares = function(arr) {
-    let newArr = [];
-  arr = arr.sort((a, b) => a + b);
+   arr = arr.sort((a, b) => a + b);
   for (let i = 0; i < arr.length; i++) {
-    newArr.push(arr[i] * arr[i]);
+    arr[i] = arr[i] * arr[i];
   }
-  return newArr;
+  return arr;
 };
