556ef041001506c224a0de0ac8d4123fc4eb9764
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-01-02 22:01:09 +0500

    Time: 7 ms (73.78%), Space: 54.4 MB (78.85%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(str) {
+      let left = 0;
+  let right = str.length - 1;
+  const vowels = new Set("aeiouAEIOU");
+  const arr = str.split("");
+
+  while (left < right) {
+    if (!vowels.has(arr[left])) {
+      left++;
+      continue;
+    }
+    if (!vowels.has(arr[right])) {
+      right--;
+      continue;
+    }
+
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+
+  return arr.join("");
+};
