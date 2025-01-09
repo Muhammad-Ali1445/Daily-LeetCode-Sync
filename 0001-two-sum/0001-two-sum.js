584e9b4b2947e5817ee67c51ee3836c1dc0e8dff
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-01-09 15:56:13 +0500

    Time: 2 ms (75.8%), Space: 51.6 MB (24.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(arr, target) {
-     for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === target) {
-        return [i, j];
-      }
+     let map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    let complement = target - arr[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
     }
+    map.set(arr[i], i);
   }
 };
