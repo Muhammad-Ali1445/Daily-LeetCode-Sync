24f0d7102665f2dd38170b1cf86ac02fec9391b7
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-01-11 12:10:28 +0500

    Time: 2 ms (76.2%), Space: 59.3 MB (45.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let min_price = Infinity;
-let maxProfit = 0;
-for (let price of prices) {
-  if (price < min_price) {
-    min_price = price;
-  }
-  let profit = price - min_price;
-  if (profit > maxProfit) {
-    maxProfit = profit;
-  }
-}
-return maxProfit;
+    let buy = prices[0];
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < buy) {
+            buy = prices[i];
+        } else if (prices[i] - buy > maxProfit) {
+            maxProfit = prices[i] - buy;
+        }
+    }
+    return maxProfit;
 }
