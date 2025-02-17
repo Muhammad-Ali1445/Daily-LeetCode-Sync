788451541ec531fcb6835d35aa757b5c4bc7c069
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-02-17 05:44:29 +0500

    Time: 0 ms (100%), Space: 52.5 MB (86.28%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+     let start = 0;
+  let end = nums.length - 1;
+  let mid;
+  while (start <= end) {
+    mid = Math.floor(start + (end - start) / 2);
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target < nums[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+};
