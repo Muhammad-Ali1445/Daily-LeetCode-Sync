de0a9c497db2af65b4a8e0f33b357955f68326c6
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-02-20 09:21:25 +0500

    Time: 0 ms (100%), Space: 64.3 MB (6.38%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
@@ -2,17 +2,21 @@
  * @param {number[]} arr
  * @return {number}
  */
-var peakIndexInMountainArray = function (arr) {
-     let start = 0;
-  let end = arr.length - 1;
-  let mid;
-  while (start < end) {
-    mid = Math.floor((start + end) / 2);
-    if (arr[mid] < arr[mid + 1]) {
-      start = mid + 1;
-    } else {
-      end = mid;
+var peakIndexInMountainArray = function (nums) {
+    let start = 1;
+    let end = nums.length - 2;
+    let mid;
+    while (start <= end) {
+        mid = Math.floor(start + (end - start) / 2);
+        if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+            return mid;
+        }
+
+        if (nums[mid] > nums[mid - 1]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
-  }
-  return start;
+    return start;
 };
