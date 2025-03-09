70ed565eb6fdc6f98acb5eeb2f1a3f9aa81ee12b
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-03-09 14:25:00 +0500

    Time: 7 ms (64.37%), Space: 55.7 MB (14.58%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,33 +4,32 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    if (s.length !== t.length) {
+     if (s.length !== t.length) {
+    return false;
+  }
+
+  let map_s_to_t = {};
+  let map_t_to_s = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let s_char = s[i];
+    let t_char = t[i];
+
+    if (map_s_to_t.hasOwnProperty(s_char)) {
+      if (map_s_to_t[s_char] !== t_char) {
         return false;
+      }
+    } else {
+      map_s_to_t[s_char] = t_char;
     }
 
-    const map_s_to_t = {};
-    const map_t_to_s = {};
-
-    for (let i = 0; i < s.length; i++) {
-        const s_char = s[i];
-        const t_char = t[i];
-
-        if (map_s_to_t.hasOwnProperty(s_char)) {
-            if (map_s_to_t[s_char] !== t_char) {
-                return false;
-            }
-        } else {
-            map_s_to_t[s_char] = t_char;
-        }
-
-        if (map_t_to_s.hasOwnProperty(t_char)) {
-            if (map_t_to_s[t_char] !== s_char) {
-                return false;
-            }
-        } else {
-            map_t_to_s[t_char] = s_char;
-        }
+    if (map_t_to_s.hasOwnProperty(t_char)) {
+      if (map_t_to_s[t_char] !== s_char) {
+        return false;
+      }
+    } else {
+      map_t_to_s[t_char] = s_char;
     }
-
-    return true;
+  }
+  return true;
 };
