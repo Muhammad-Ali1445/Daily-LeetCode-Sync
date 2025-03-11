c9dea76cfdc13f15625d7ed412b8fe966f05aa5d
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-03-11 22:57:28 +0500

    Time: 29 ms (5.06%), Space: 62.8 MB (5.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -3,18 +3,18 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function(arr1, arr2) {
-      let newArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    let found = -1;
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] == arr2[j]) {
-        found = arr2.slice(j + 1).find((element) => element > arr2[j]);
-        newArr.push(found !== undefined ? found : -1);
-        break;
-      }
+var nextGreaterElement = function (arr1, arr2) {
+    let newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let found = -1;
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] == arr2[j]) {
+                found = arr2.slice(j + 1).find((element) => element > arr2[j]);
+                newArr.push(found !== undefined ? found : -1);
+                break;
+            }
+        }
     }
-  }
 
-  return newArr;
+    return newArr;
 };
