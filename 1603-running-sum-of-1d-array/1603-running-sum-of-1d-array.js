b3e8fb6076eda7b703e2190241ae006433abec00
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-07-31 15:26:21 +0500

    Time: 0 ms (100%), Space: 55.2 MB (30.14%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -2,11 +2,11 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(arr) {
-     let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-    arr[i] = sum;
-  }
-  return arr;
+var runningSum = function (nums) {
+    let sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        nums[i] = sum
+    }
+    return nums
 };
