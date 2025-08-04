61d4725775fe0b7d208bc9247aefcf417446cb45
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-04 07:48:40 +0500

    Time: 2 ms (42.81%), Space: 58 MB (52.85%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -2,9 +2,9 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function (num) {
+var fizzBuzz = function (n) {
     let resltArr = [];
-    for (let i = 1; i <= num; i++) {
+    for (let i = 1; i <= n; i++) {
         let string = "";
         if (i % 3 === 0) string += "Fizz";
         if (i % 5 === 0) string += "Buzz";
