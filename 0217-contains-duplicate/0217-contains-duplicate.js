c448a94a72365da2ff4446958436d2e443c32240
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-05 20:44:08 +0500

    Time: 17 ms (70.73%), Space: 73.4 MB (74.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,10 +2,10 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {
-    let numbers = new Set()
-    for (let i = 0; i < nums.length; i++) {
-        if (numbers.has(nums[i])) {
+var containsDuplicate = function(nums) {
+    let numbers=new Set()
+    for(let i=0;i<nums.length;i++){
+        if(numbers.has(nums[i])){
             return true
         }
         numbers.add(nums[i])
