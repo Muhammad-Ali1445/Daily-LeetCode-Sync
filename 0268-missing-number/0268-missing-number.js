2d871d86f49c232c9b568a52d5584c40a552656d
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-05 20:36:21 +0500

    Time: 9 ms (27.94%), Space: 58 MB (17.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,15 +3,29 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    let map = {};
-
+    let map = {}
     for (let num of nums) {
-        map[num] = true;
+        map[num] = true
     }
 
     for (let i = 0; i <= nums.length; i++) {
         if (!map.hasOwnProperty(i)) {
-            return i;
+            return i
         }
     }
 };
+
+
+// Second Method 
+
+// let range = nums.length;
+// let expectedSum = 0;
+// let actualSum = 0;
+// for (let i = 0; i <= range; i++) {
+//     expectedSum += i;
+// }
+// for (let i = 0; i < nums.length; i++) {
+//     actualSum += arr[i];
+// }
+// let missingNum = expectedSum - actualSum;
+// return missingNum;
