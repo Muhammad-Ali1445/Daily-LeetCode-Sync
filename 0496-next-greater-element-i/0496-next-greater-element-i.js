8cc94174da49f64b14536c83a7708176c6d0d9f9
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-10 21:20:58 +0500

    Time: 9 ms (13.73%), Space: 58.5 MB (10.7%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -3,15 +3,15 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function (nums1, nums2) {
-    let map = new Map();
-  let stack = [];
-
-  for (let num of nums2) {
-    while (stack.length > 0 && stack[stack.length - 1] < num) {
-      map.set(stack.pop(), num);
+var nextGreaterElement = function(nums1, nums2) {
+    let map=new Map();
+    let stack=[];
+    for(let num of nums2){
+        while(stack.length>0 && stack[stack.length-1]<num){
+            map.set(stack.pop(),num)
+        }
+        stack.push(num)
     }
-    stack.push(num);
-  }
-  return nums1.map((num) => map.get(num) ?? -1);
+    return nums1.map((num)=>map.get(num)?? -1)
+
 };
