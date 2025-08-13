7c9c99a38b9c75d5e22b6bd7a4c8ed27dd436ee2
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-13 09:06:02 +0500

    Time: 0 ms (100%), Space: 57.5 MB (55.19%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,25 +9,14 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-
- // ------- Iteratively --------
 var reverseList = function (head) {
-    let prev = null;
+    let prev = null
     let curr = head
     while (curr !== null) {
-        let next = curr.next
+        const next = curr.next
         curr.next = prev
         prev = curr
         curr = next
     }
     return prev
 };
-
-// --------- Recursively -----------
-
-// var reverseList = function (head,prev=null) {
-//    if(head===null) return prev
-//    let next=head.next
-//    head.next=prev
-//    return reverseList(next,head)
-// };
