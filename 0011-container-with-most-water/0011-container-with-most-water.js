3472ce3474cecb67940a3c7130650cdf5cc47c2f
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-25 11:40:23 +0500

    Time: 3 ms (54.35%), Space: 62.7 MB (62.84%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -2,21 +2,21 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(nums) {
+var maxArea = function (height) {
     let left = 0;
-  let right = nums.length - 1;
-  let maxArea = 0;
-  let width;
-  let currArea;
-  while (left < right) {
-    width = right - left;
-    currArea = Math.min(nums[left], nums[right]) * width;
-    maxArea = Math.max(maxArea,currArea);
-    if (nums[left] < nums[right]) {
-      left++;
-    } else {
-      right--;
+    let right = height.length - 1;
+    let maxArea = 0;
+    let width;
+    let currArea;
+    while (left < right) {
+        width = right - left;
+        currArea = Math.min(height[left], height[right]) * width;
+        maxArea = Math.max(maxArea, currArea);
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
     }
-  }
-  return maxArea;
+    return maxArea;
 };
