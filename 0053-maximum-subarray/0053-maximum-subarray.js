d3440b06a2218187b8986e8783ceb65f558a4a53
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-27 20:37:05 +0500

    Time: 5 ms (16.38%), Space: 65.4 MB (13.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let maxSum = nums[0];
-    let currSum = 0;
-    for (let value of nums) {
-        currSum += value;
+    let maxSum = nums[0]
+    let currSum=0
+    for (let val of nums) {
+        currSum += val
         if (currSum > maxSum) {
-            maxSum = currSum;
+            maxSum = currSum
         }
 
         if (currSum < 0) {
-            currSum = 0;
+            currSum = 0
         }
     }
-    return maxSum;
+    return maxSum
 };
