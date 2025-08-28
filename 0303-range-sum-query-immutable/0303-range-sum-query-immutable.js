10873a54da40b719061519673d41d96a58eaef43
Ali Munir <108221222+Muhammad-Ali1445@users.noreply.github.com>
2025-08-28 08:59:48 +0500

    Time: 77 ms (22.43%), Space: 63.2 MB (47.68%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -23,3 +23,20 @@ NumArray.prototype.sumRange = function (left, right) {
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(left,right)
  */
+
+ // second Method -- Optimized ----
+
+//  var NumArray = function(nums) {
+//     // prefixSum[i] = sum of nums[0...i-1]
+//     this.prefixSum = new Array(nums.length + 1).fill(0);
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         this.prefixSum[i + 1] = this.prefixSum[i] + nums[i];
+//     }
+// };
+
+// NumArray.prototype.sumRange = function(left, right) {
+//     // sum = prefix[right+1] - prefix[left]
+//     return this.prefixSum[right + 1] - this.prefixSum[left];
+// };
+ 
